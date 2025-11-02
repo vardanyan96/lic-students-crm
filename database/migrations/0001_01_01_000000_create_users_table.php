9c958e7925d9980bff6d54d44e0f205b23a73cba
Narek Vardanyan <vardanyan.work5@gmail.com>
2025-11-02 20:57:45 +0400

    fix

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('surname');
-            $table->string('patronymic');
+            $table->string('surname')->nullable();
+            $table->string('patronymic')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
